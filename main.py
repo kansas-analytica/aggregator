320cd9ad96eafe870f559a147a6ccdb5bce10eee
Patrick Canny <patrickcanny@ku.edu>
2019-04-03 20:25:18 -0500

    create route

--- main.py
+++ main.py
@@ -3,15 +3,17 @@
 import os
 import string
 import tweepy
-import tools
 import json
 from flask import Flask, request, logging
 from dotenv import load_dotenv
+from tools import BoNTools
 
 import MySQLdb # For MySQL connection
 from datetime import datetime # For timestamp parsing
 
 # global variables
+tools = BoNTools()
+DEMO = False
 
 # load dotenv
 APP_ROOT = os.path.join(os.path.dirname(__file__), '.')
@@ -29,6 +31,16 @@ db_host = os.getenv('DB_HOST')
 db_user = os.getenv('DB_USER')
 db_pass = os.getenv('DB_PASS')
 db_name = os.getenv('DB_NAME')
+
+
+# Connect to Database
+db = MySQLdb.connect(host=db_host,
+                     user=db_user,
+                     passwd=db_pass,
+                     db=db_name,
+                     charset='utf8',
+                     use_unicode=True)
+KA = db.cursor()
 
 # setup tweepy
 auth = tweepy.OAuthHandler(consumer_key, consumer_sec)
@@ -58,7 +70,7 @@ def aggregate_by_user(user_id, count):
 
 @app.route("/tweets/bulk", methods=['GET'])
 def aggregate_bulk():
-
+    users = tools.build_user_list()
 
 # END Routes
 
@@ -71,84 +83,77 @@ if __name__ == '__main__':
    LOGGER.info('Twitter Consumer Secret: {}'.format(consumer_sec))
    LOGGER.info('Twitter Access Token: {}'.format(access_tok))
    LOGGER.info('Twitter Access Token Secret: {}'.format(access_tok_sec))
+   app.run(host = '0.0.0.0', port = port)
 
    # NOTE For demonstration purposes
    # We give a list of users to perform aggregate_by_user on to populate the database with some base values.
 
-   #Connect to Database
-   db = MySQLdb.connect(host=db_host,
-                        user=db_user,
-                        passwd=db_pass,
-                        db=db_name,
-                        charset='utf8',
-                        use_unicode=True)
-   KA = db.cursor()  # Cursor for interacting with the Kansas Analytica database
 
-   #realDonaldTrump, BarackObama, HillaryClinton
-   users_to_populate = ['25073877',
-                        '813286',
-                        '1339835893',
-                        '3320478908',
-                        '38002432',
-                        '1636590253',
-                        '30364057',
-                        '818927131883356161',
-                        '759251',
-                        '1367531',
-                        '1407822289']
+   ##realDonaldTrump, BarackObama, HillaryClinton
+   #users_to_populate = ['25073877',
+   #                     '813286',
+   #                     '1339835893',
+   #                     '3320478908',
+   #                     '38002432',
+   #                     '1636590253',
+   #                     '30364057',
+   #                     '818927131883356161',
+   #                     '759251',
+   #                     '1367531',
+   #                     '1407822289']
 
-   # format each and place in database.
-   for user in users_to_populate:
-       user_tweets = aggregate_by_user(user, 15)
-       user_tweets = user_tweets.encode('utf-8')
-       tweet_info = json.loads(user_tweets)
+   ## format each and place in database.
+   #for user in users_to_populate:
+   #    user_tweets = aggregate_by_user(user, 15)
+   #    user_tweets = user_tweets.encode('utf-8')
+   #    tweet_info = json.loads(user_tweets)
 
-       # Insert the user info into the database
-       try:
-           t_time = datetime.strptime(tweet_info['0']['user']['created_at'], '%a %b %d %H:%M:%S %z %Y')
-           t_time = datetime.strftime(t_time, '%Y-%m-%d %H:%M:%S')
-       except:
-           t_time = datetime.now()
+   #    # Insert the user info into the database
+   #    try:
+   #        t_time = datetime.strptime(tweet_info['0']['user']['created_at'], '%a %b %d %H:%M:%S %z %Y')
+   #        t_time = datetime.strftime(t_time, '%Y-%m-%d %H:%M:%S')
+   #    except:
+   #        t_time = datetime.now()
 
 
-       # Insert the account into the twitter_accounts table
-       query = "INSERT IGNORE INTO twitter_accounts (id, name, screen_name, description, date_created, followers, following, image_url) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-       values = (tweet_info['0']['user']['id'],
-            tweet_info['0']['user']['name'],
-            tweet_info['0']['user']['screen_name'],
-            tweet_info['0']['user']['description'],
-            t_time,
-            tweet_info['0']['user']['followers_count'],
-            tweet_info['0']['user']['friends_count'],
-            tweet_info['0']['user']['profile_image_url'])
+   #    # Insert the account into the twitter_accounts table
+   #    query = "INSERT IGNORE INTO twitter_accounts (id, name, screen_name, description, date_created, followers, following, image_url) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+   #    values = (tweet_info['0']['user']['id'],
+   #         tweet_info['0']['user']['name'],
+   #         tweet_info['0']['user']['screen_name'],
+   #         tweet_info['0']['user']['description'],
+   #         t_time,
+   #         tweet_info['0']['user']['followers_count'],
+   #         tweet_info['0']['user']['friends_count'],
+   #         tweet_info['0']['user']['profile_image_url'])
 
 
-       # LOGGER.info('ADDING User to database: \n{}'.format(tweet_info))
-       LOGGER.debug('\nQUERY: {}\n'.format(query))
-       LOGGER.debug('\nVALUES: {}\n'.format(values))
+   #    # LOGGER.info('ADDING User to database: \n{}'.format(tweet_info))
+   #    LOGGER.debug('\nQUERY: {}\n'.format(query))
+   #    LOGGER.debug('\nVALUES: {}\n'.format(values))
 
-       # Execute query and commit to database
-       KA.execute(query, values)
-       db.commit()
+   #    # Execute query and commit to database
+   #    KA.execute(query, values)
+   #    db.commit()
 
-       # Insert the tweets into the tweets table
-       for tweet in tweet_info:
-           LOGGER.debug("TWEET: {}".format(tweet))
-           try:
-             t_time = datetime.strptime(tweet_info[tweet]["created_at"], "%a %b %d %H:%M:%S %z %Y")
-             t_time = datetime.strftime(t_time, '%Y-%m-%d %H:%M:%S')
-           except:
-             t_time = datetime.now()
-           t_id_str = tweet_info[tweet]['id_str']
-           t_text = tweet_info[tweet]['text']
-           t_user_id = tweet_info[tweet]['user']['id']
+   #    # Insert the tweets into the tweets table
+   #    for tweet in tweet_info:
+   #        LOGGER.debug("TWEET: {}".format(tweet))
+   #        try:
+   #          t_time = datetime.strptime(tweet_info[tweet]["created_at"], "%a %b %d %H:%M:%S %z %Y")
+   #          t_time = datetime.strftime(t_time, '%Y-%m-%d %H:%M:%S')
+   #        except:
+   #          t_time = datetime.now()
+   #        t_id_str = tweet_info[tweet]['id_str']
+   #        t_text = tweet_info[tweet]['text']
+   #        t_user_id = tweet_info[tweet]['user']['id']
 
-           LOGGER.info('ADDING Tweet to database: \nuser_id: {} | Created At: {} | id_str: {} | text: {}'.format(t_user_id, t_time, t_id_str, t_text))
+   #        LOGGER.info('ADDING Tweet to database: \nuser_id: {} | Created At: {} | id_str: {} | text: {}'.format(t_user_id, t_time, t_id_str, t_text))
 
-           query = "INSERT IGNORE INTO tweets (created_at, id_str, text, user_id) VALUES (%s, %s, %s, %s)"
-           values = (t_time, t_id_str, t_text, t_user_id)
+   #        query = "INSERT IGNORE INTO tweets (created_at, id_str, text, user_id) VALUES (%s, %s, %s, %s)"
+   #        values = (t_time, t_id_str, t_text, t_user_id)
 
-           # Execute query and commit to database
-           KA.execute(query, values)
-           db.commit()
+   #        # Execute query and commit to database
+   #        KA.execute(query, values)
+   #        db.commit()
 
--- tools.py
+++ tools.py
@@ -5,7 +5,15 @@ import os
 from dotenv import load_dotenv
 import queue
 
-# Builds a rotating queue of API keys from the environment
-def generate_key_queue():
-    Q = queue.Queue()
+class BoNTools():
+    def __init__(self):
+        pass
 
+    # Builds a rotating queue of API keys from the environment
+    def generate_key_queue(self):
+        #TODO
+        pass
+
+    # handles getting the users we want to get tweets for in a list
+    def build_user_list(self):
+        return [os.getenv('USERS_TO_AGGREGATE').split(',')]
